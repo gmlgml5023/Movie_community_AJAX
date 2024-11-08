e6575f7afd65613af416a9a8ceaae56648403354
gahee <gmlgml5023@naver.com>
2024-11-08 11:29:26 +0900

    [movies] 영화 장르 필터링 구현

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_safe
 from .models import Movie, Genre
+from django.http import JsonResponse
 
 # Create your views here.
 @require_safe
@@ -21,8 +22,16 @@ def filter_genre(request):
     '''
     필터링된 영화 데이터 제공
     '''
-    
-    pass
+    genre_name = request.GET.get('genre', '')
+    # 장르가 선택되었으면
+    if genre_name:
+        movies = Movie.objects.filter(genres__name=genre_name).values('title')
+    else:
+        movies = Movie.objects.all().values('title')
+
+    movies_list = list(movies)  # QuerySet을 list로 변환하여 반환
+
+    return JsonResponse(movies_list, safe=False)
 
 
 @require_safe
